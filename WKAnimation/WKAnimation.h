3496bb5c23b4969b48a54d89549fb84e6f071d05
Eli Tyrant <119923856@qq.com>
2014-09-04 13:48:34 +0800

    add file

---
+++ WKAnimation/WKAnimation.h
@@ -0,0 +1,13 @@
+//
+//  WKAnimation.h
+//  WKAnimationDemo
+//
+//  Created by ZhuangXiaowei on 14-9-4.
+//  Copyright (c) 2014年 Tyrant. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WKAnimation : NSObject
+
+@end
---
+++ WKAnimation/WKAnimation.m
@@ -0,0 +1,13 @@
+//
+//  WKAnimation.m
+//  WKAnimationDemo
+//
+//  Created by ZhuangXiaowei on 14-9-4.
+//  Copyright (c) 2014年 Tyrant. All rights reserved.
+//
+
+#import "WKAnimation.h"
+
+@implementation WKAnimation
+
+@end
